6a2dbdf4aac7a44884fba4c261bd50a8bc13e598
KornSiwat <ponpued@gmail.com>
2019-06-07 22:04:47 +0700

    separated router&controll type, added Error class

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
 import express from "express";
-import { createRouter } from "./Router";
 import "reflect-metadata";
 import { createConnection } from "typeorm";
+import { createPublicRouter } from "./routers/PublicRouter";
+import { createAuthenticatedRouter} from "./routers/AuthenticatedRouter";
+import { ParkingLotApplication } from "./ParkingLotApplication";
 
 createConnection().then(connection => {
   const app = express();
   const port = 3000;
-  const router = createRouter()
+  const parkingLotApplication = new ParkingLotApplication();
+  const publicRouter = createPublicRouter(parkingLotApplication);
+  const authenticatedRouter = createAuthenticatedRouter(parkingLotApplication);
 
   app.use(express.json());
 
-  app.use(router);
+  app.use(publicRouter);
+  app.use(authenticatedRouter);
 
   app.listen(port, () =>
     console.log(`Parking lot Application is on port ${port}!`)
